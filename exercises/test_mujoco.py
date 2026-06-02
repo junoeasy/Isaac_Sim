f9ff37b65b322e74dbe666a3697a4ab66323cee9
junoeasy <thswnsgh00@naver.com>
2026-06-02 12:20:58 +0900

    vault backup: 2026-06-02 12:20:58

--- exercises/test_mujoco.py
+++ exercises/test_mujoco.py
@@ -8,7 +8,7 @@ print("obs:", obs)
 print("obs shape:", obs.shape)
 print("action space:", env.action_space)
 
-for step in range(1000):
+for step in range(10000):
     cart_pos, pole_angle, cart_vel, pole_ang_vel = obs
     action_value = (
         10.0 * pole_angle
@@ -23,7 +23,7 @@ for step in range(1000):
     obs, reward, terminated, truncated, info = env.step(action)
 
     print(step, reward, terminated, truncated,obs[0],obs[1],obs[2],obs[3])
-    time.sleep(0.1)
+
     if terminated:
         obs, info = env.reset()
         break
